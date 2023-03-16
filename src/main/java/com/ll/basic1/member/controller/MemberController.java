0245063249dd4ccc8a6053779ea83f3ec6c40462
elephantLikesApple <elephant890@naver.com>
2023-03-16 14:39:51 +0900

    use template html

--- src/main/java/com/ll/basic1/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/member/controller/MemberController.java
@@ -18,21 +18,8 @@ public class MemberController {
     private final Rq rq;
 
     @GetMapping("/member/login")
-    @ResponseBody
     public String showLogin() {
-        if(rq.isLogined()) {
-            return """
-                    <h1>이미 로그인 되었습니다. </h1>
-                    """.stripIndent();
-        }
-        return """
-                <h1>로그인</h1>
-                <form action="doLogin">
-                  <input type="text" name="username" placeholder="아이디" />
-                  <input type="password" name="password" placeholder="비밀번호" />
-                  <button type="submit">로그인</button>
-                </form>
-                    """.stripIndent();
+        return "/usr/member/login.html";
     }
 
     @GetMapping("/member/doLogin")
