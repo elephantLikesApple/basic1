10a9889eb910cb70d15e4794391086c34dda3b39
elephantLikesApple <elephant890@naver.com>
2023-03-17 15:49:39 +0900

    auto create date

--- src/main/java/com/ll/basic1/article/entity/Article.java
+++ src/main/java/com/ll/basic1/article/entity/Article.java
@@ -1,12 +1,16 @@
 package com.ll.basic1.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -17,11 +21,14 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private long id;
+    @CreatedDate
     private LocalDateTime createDate; // 생성 날짜
+    @LastModifiedDate
     private LocalDateTime modifyDate; // 수정 날짜
     private String title;
     private String body;
